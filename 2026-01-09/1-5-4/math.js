a2ba9fee73507261aebc80445ba8c97384b2ee27
kimsinjung189 <kimsinjung189@gamail.com>
2026-01-14 00:40:23 +0900

    export+import (모듈 module)

--- 2026-01-09/1-5-4/math.js
+++ 2026-01-09/1-5-4/math.js
@@ -1,7 +1,7 @@
-function add(a, b) {
-            return a + b;
-        }
+export function add(a,b) {
+    return a + b;
+}
 
-        function sub(a, b) {
-            return a - b;
-        }
+export function sub(a,b) {
+    return a - b;
+}
